24d5084e3ef709d03012f9fa7e8d61f747d8dd03
Oscar Carlsson <ocarlsson3@gmail.com>
2013-12-09 16:17:49 +0100

    Init structure src

---
+++ src/run.py
@@ -0,0 +1,19 @@
+
+
+# Compile #
+
+# Compile Agent
+# Compile Experiment
+# Compile all Environments
+#  with makefiles??
+
+# Run #
+# One environment-test
+    # Start RL_GLUE
+    # Start Experiment
+    # Start AGENT
+    # Run one environment in /environtments
+
+    # Print/Save output
+    # at least experiments..
+
