a324b6a488cb76b171f849b1f91d261671a5ea09
Oscar Carlsson <ocarlsson3@gmail.com>
2013-12-11 21:55:22 +0100

    Plot all results in directory.

--- src/plotresults.py
+++ src/plotresults.py
@@ -1,30 +1,29 @@
-#!/usr/bin python
+#!/usr/bin/env python
 import pandas as pd
 import matplotlib.pyplot as plt
+import argparse
+import os
 
-csvfile = 'result'
-#csvfile = '../outputs/131211-15-54-28-LibRLAgent/resultMyEnvironment'
-csvdata = pd.read_csv(csvfile, index_col=0, header=None, names=range(0,525,25))
+def plot(csvfile):
+    csvdata = pd.read_csv(csvfile, index_col=0, header=None, names=range(0,525,25))
+    meandata = csvdata.loc['mean']
+    if meandata.ndim == 1:
+        meandata.plot()
+    else:
+        meandata.mean().plot()
 
-#print csvdata[0].loc['mean']
-#print csvdata[0].loc['mean'].mean()
-meandata = csvdata.loc['mean']
-meandata.mean().plot()
+parser = argparse.ArgumentParser(description='Plotting the average mean of a result-output. Argument is the directory')
+parser.add_argument('-D', metavar='dir', required=True,
+                   help="Directory where result files are found.",default=None)
+args = parser.parse_args()
+
+results = [filename for filename in os.listdir(args.D) if filename.startswith('result')]
 plt.ylabel('#Episodes')
 plt.xlabel('Average reward')
 plt.title('Environment z with x runs using agent y')
-#print csvdata.index
-
-#print csvdata.loc['mean']
-#print csvdata.loc['std']
-
-#csvdata.loc['mean'].plot()
+plt.figure()
+print results
+for result in results:
+    plot(args.D+"/"+result)
+plt.legend(results)
 plt.show()
-
-# run
-# same Environment
-# same Agent
-# same Experiment
-# n times
-
-# => 100 csv, take mean, plot
