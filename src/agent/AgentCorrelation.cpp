7784bce6bf639d06d554d0c86de94e15f12adf30
huskarl <oskarxvi@gmail.com>
2013-12-12 00:33:48 +0100

    AgentCorrelation
    contains functinons for producing relative prob-weight of different
    actions based on history. Loads of input and a pointer output, want to
    discuss details.

---
+++ src/agent/AgentCorrelation.cpp
@@ -0,0 +1,206 @@
+#include "Agent.hpp"
+
+#include <math.h>
+#include <cstdlib>
+#include <cstdio>
+#include <cassert>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <sstream>      
+#include <time.h>
+#include <cfloat>
+
+using namespace std;
+
+int randInRange(int max);
+double runif()
+{
+    return ((double)rand() / ((double)(RAND_MAX)+(double)(1)));
+}
+
+Agent::Agent(int nStates, int nActions, double gamma, double lambda,
+             double stepSize, double epsilon)
+: nStates(nStates), nActions(nActions), gamma(gamma), lambda(lambda),
+  stepSize(stepSize), t(0), e(0), policy(epsilon) /* EpsilonGreedyPolicy(epsilon) */
+{
+    srand(time(0));
+    qTable = new double*[nStates];
+    traces = new double*[nStates];
+    for (int s = 0; s < nStates; ++s)
+    {
+        qTable[s] = new double[nActions];
+        traces[s] = new double[nActions];
+    }
+    rTable = new double*[nActions]; //Reward table
+    cTable = new double*[nActions]; //Tally table
+    for (int a = 0; a<nActions; ++a)
+    {
+        reward_correlation_table[a] = new double[nActions];
+        tally_correlation_table = new double[nActions];
+    }
+    cout << "Initializing agent with parameters:" << endl
+              << "\tnStates : "  << nStates  << endl
+              << "\tnActions : " << nActions << endl
+              << "\tgamma : "    << gamma    << endl
+              << "\tlambda : "   << lambda   << endl
+              << "\tepsilon : "   << epsilon   << endl;
+}
+
+Agent::~Agent()
+{
+    std::cout << "~Agent()" << endl;
+    for (int i = 0; i < nStates; ++i)
+    {
+        delete [] qTable[i];
+        delete [] traces[i];
+    }
+    for (int a = 0; a<nActions; ++a)
+    {
+        delete [] rTable[a];
+        delete [] cTable[a];
+    }
+}
+
+/*
+ * This method implements the Tabular Sarsa(lambda) from Figure 7.11 (Sutton)
+ */
+int Agent::step(int lastState, int lastAction, double reward, int thisState)
+{
+    const int history_size = 5000;
+    const int history_incr = 1000;
+    static vector<int> history_S(history_size);
+    static vector<int> history_A(history_size);
+
+    // Take action A, observe R, S'
+    // ... is given already as:
+    int S = lastState;
+    int A = lastAction;
+    int S2= thisState;  // S'
+
+    if (t == history_S.capacity())
+        history_S.resize(t + history_incr);
+    if (t == history_A.capacity())
+        history_A.resize(t + history_incr);
+
+    history_S[t] = S;
+    history_A[t] = A;
+
+    // Choose A2 from S2 using policy derived from Q (e.g. epsilon-greedy)
+    int A2 = policy.sample_action(S2, t, qTable, nActions);
+
+    double delta = reward + gamma * qTable[S2][A2] - qTable[S][A];
+    traces[S][A] += 1;
+
+    for (int ti = 0; ti <= t; ++ti)
+    {
+        int s = history_S[ti];
+        int a = history_A[ti];
+        qTable[s][a] += stepSize * delta * traces[s][a];
+        traces[s][a] = gamma * lambda * traces[s][a];
+    }
+
+    ++t;
+    return A2;
+}
+
+void Agent::start()
+{
+    t = 0;
+    ++e;
+    cout << "\t" << e << "\r";
+    cout.flush();
+    for (int s = 0; s < nStates; ++s)
+        for (int a = 0; a < nActions; ++a)
+        {
+            traces[s][a] = 0;
+        }
+}
+
+double * expectationFromCorrelations(double * actionExpt, double ** rTable,
+                                     double **cTable, double gamma,
+                                     int nActions, static vector<int> history_A,
+                                     int t)
+// sort it into Agent:: or policy?
+{
+    double total = 0;
+    for (int a = 0; a<nActions; ++a)
+    {
+        actionExpt[a] = 0;
+        double pow = 1;
+        for (int h = t-1; h>=0; --h)
+        {
+            actionExpt[a] += pow * rTable[a][history_A[h]]/cTable[a][history_A[h]];
+            pow *= gamma;
+        }
+        total+= actionExpt[a];
+    }
+
+    for (int a = 0; a<nActions; ++a)
+    {
+        actionExpt[a]/=total;
+    }
+    
+    return actionExpt;
+    
+}
+
+void updateCorrelationMatrices(double lastReward, double ** rTable,
+                               double **cTable, double gamma,
+                               int nActions, static vector<int> history_A,
+                               int t)
+    //gamma as a pointer, so it updates as well...
+{
+    double pow = 1;
+    for (int h = t-2; h>0; h--)
+    {
+        double powpow = pow;
+        for (int hh = h-1; hh >= 0; hh--) 
+        {
+            rTable[a][history_A[h]] += powpow*pow*lastReward; //discount: gamma^(most distant action)
+            cTable[a][history_A[h]] += powpow*pow;
+            powpow*= gamma;
+        }
+    }
+    
+    // FIX GAMMA UPDATER WHEN REST WORKS
+   /* double exp0[nActions];
+    double exp1[nActions];
+    
+    exp0 = expectationFromCorrelations(* exp0, **rTable,
+                                **cTable, gamma,
+                                nActions, history_A,
+                                               t-1);
+    exp1 = expectationFromCorrelations(* exp1, **rTable,
+                                               **cTable, gamma+0.001,
+                                               nActions, history_A,
+                                               t-1);
+    gamma += (lastReward-exp0)/(exp1-exp0)*/ 
+    
+}
+
+EpsilonGreedyPolicy::EpsilonGreedyPolicy(double epsilon)
+    : epsilon(epsilon)
+{}
+
+int EpsilonGreedyPolicy::sample_action(int S, int t, double **qTable,
+                                       int nActions)
+{
+    // Ignore time, but could use epsilon/t.
+    if (runif() < epsilon)
+        return randInRange(nActions-1);
+
+    int aMax;
+    double qMax = -DBL_MAX;
+    for (int a = 0; a < nActions; ++a)
+    {
+        if (qTable[S][a] > qMax)
+        {
+            qMax = qTable[S][a];
+            aMax = a;
+        }
+    }
+
+    return aMax;
+}
+
