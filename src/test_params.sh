e5d413cccd38c783de58c81ff596511025f31ca7
johnkarlsson <tartrate@gmail.com>
2013-12-15 02:02:04 +0100

    Wrong param order, fixed

--- src/test_params.sh
+++ src/test_params.sh
@@ -2,8 +2,8 @@ if [ $# != 3 ]; then
     echo "Usage: ./test_params.sh <lambda> <stepsize> <N>"
     return
 fi
-export LIBRLAGENT_STEPSIZE=$1
-export LIBRLAGENT_LAMBDA=$2
+export LIBRLAGENT_LAMBDA=$1
+export LIBRLAGENT_STEPSIZE=$2
 ./run.py -N $3 -E environments/tictactoe/TictactoeEnvironment
 unset LIBRLAGENT_LAMBDA
 unset LIBRLAGENT_STEPSIZE
