d98a291873e58615ffdbe75fe308318a96bbd526
johnkarlsson <tartrate@gmail.com>
2013-12-15 11:06:23 +0100

    test_params uses christos experiment

--- src/test_params.sh
+++ src/test_params.sh
@@ -4,6 +4,6 @@ if [ $# != 3 ]; then
 fi
 export LIBRLAGENT_LAMBDA=$1
 export LIBRLAGENT_STEPSIZE=$2
-./run.py -N $3 -E environments/tictactoe/TictactoeEnvironment
+./run.py -N $3 -E environments/tictactoe/TictactoeEnvironment -X ChristosExperiment
 unset LIBRLAGENT_LAMBDA
 unset LIBRLAGENT_STEPSIZE
