dd180eea63f5b353983bf608174e8bc6a6a9f9fe
Oscar Carlsson <ocarlsson3@gmail.com>
2013-12-18 12:35:50 +0100

    Update lambdaplot to plot finaloutput

--- src/lambdaplot.py
+++ src/lambdaplot.py
@@ -1,14 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import sys
 
-csvfile = '../data/lambdadataa'
+# ../outputs/131218-12-27-39-LibRLAgent-Episod100/finalOutput
+csvfile = sys.argv[1] 
 
 data = pd.read_table(csvfile, sep=' ', header=None, index_col=[0,1])
+print data
 indexes = set([a for (a,b) in data.index])
+print indexes
 
 for index in indexes:
     plotdata = data.loc[index]
-    plotdata[2].plot()
+    plotdata[3].plot()
 
 plt.xlabel("Lambda")
 plt.ylabel("Cumulative reward")
